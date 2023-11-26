91d69815446bed36d7ae5242debab7db8575cbda
hugopracticas <hugoloyolamaya@gmail.com>
2023-11-26 00:33:56 -0600

    add routes in navbar

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,20 +1,19 @@
 import React from 'react'
-import { Routes, Route, Navigate } from 'react-router-dom'
-import { MarvelPage } from '../heroes/pages/MarvelPage'
-import { LoginPage } from '../auth/pages/LoginPage'
-import { DcPage } from '../heroes/pages/DcPage'
+import { Routes, Route } from 'react-router-dom'
+import { LoginPage } from '../auth'
+import { HeroesRoutes } from '../heroes'
 
 export const AppRouter = () => {
   return (
     <>
       <Routes>
-        <Route path='marvel' element={<MarvelPage />} />
-        <Route path='dc' element={<DcPage  />}/>
 
         <Route path='login' element={<LoginPage/>}/>
 
-        <Route path='/' element={<Navigate to='/marvel'/>}/>
+        <Route path='/*' element={<HeroesRoutes />}/>
+ 
       </Routes>
     </>
   )
 }
+ 
